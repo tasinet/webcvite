ea6a520ff46ecc329af331eaba77bf7895007c6e
Tasos Bitsios <tasos.bitsios@gree.net>
2013-05-20 11:16:28 +0100

    changed default json data file

--- gen.php
+++ gen.php
@@ -3,7 +3,7 @@
 include_once 'templates/header.php';
 include_once 'templates/footer.php';
 
-$data = json_decode( file_get_contents("chris_cv.json"), true );
+$data = json_decode( file_get_contents("tas_cv.json"), true );
 
 tpl_header($data);
 
